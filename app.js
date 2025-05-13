e9ee4c6819f7a9ba3fef6bd79b1e5b15f94da8b2
Cristin021 <dayanamoya021@gmail.com>
2025-05-12 19:09:39 -0500

    comit2

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+// Rutas
+app.use('/api/productos', require('./routes/productoRoutes'));
+app.use('/api/usuarios', require('./routes/usuarioRoutes'));
+app.use('/api/ordenes', require('./routes/ordenRoutes'));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(Servidor corriendo en puerto ${PORT});
+});
